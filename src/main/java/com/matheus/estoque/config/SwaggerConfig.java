4c5b84e8c778c8cf05818240f3af236f45b2c0bc
Matheus Samuel <matheussamuel05@gmail.com>
2026-06-15 23:25:03 -0300

    Arrumando deploy*

--- src/main/java/com/matheus/estoque/config/SwaggerConfig.java
+++ src/main/java/com/matheus/estoque/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,11 +21,15 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(
                         new Info()
-                                .title("Sistema de Estoque API")
+                                .title("Sistema de Gestão de Estoque")
                                 .version("1.0")
-                                .description(
-                                        "API REST desenvolvida com Spring Boot para gerenciamento de estoque."
-                                )
+                                .description("API REST desenvolvida com Spring Boot para gerenciamento de estoque.")
+                )
+                .servers(
+                        List.of(
+                                new Server()
+                                        .url("https://sistema-gestao-estoque-api-production.up.railway.app")
+                        )
                 )
                 .addSecurityItem(
                         new SecurityRequirement()
